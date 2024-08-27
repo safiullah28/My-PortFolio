77d138879e16d939e3ec6dd9c5e0bac0a44b7942
safiullah28 <safiullah424@gmail.com>
2024-08-27 18:12:03 +0500

    picture changed

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,42 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import styles from "./Navbar.module.css";
-import close from './nav/closeIcon.png';
-import menuIcon from './nav/menuIcon.png';
+import close from "./nav/closeIcon.png";
+import menuIcon from "./nav/menuIcon.png";
 const Navbar = () => {
-  const [menuOpen,setMenuopen]=useState(false);
+  const [menuOpen, setMenuopen] = useState(false);
 
   return (
     <nav className={styles.navbar}>
-      <a className={styles.title} href="/">PortFolio</a>
+      <a className={styles.title} href="/">
+        PortFolio
+      </a>
       <div className={styles.menu}>
-        <img src={menuOpen ? close : menuIcon} alt="menu-button" className={styles.menuBtn} onClick={()=>{setMenuopen(!menuOpen)}}/>
-        <ul className={`${styles.menuitems} ${menuOpen && styles.menuOpen}`} onClick={()=>{setMenuopen(false)}}>
-          <li><a href="#about">About</a></li>
-          <li><a href="#experience">Experience</a></li>
-          <li><a href="#project">Project</a></li>
-          <li><a href="#contact">Contact</a></li>
+        <img
+          src={menuOpen ? close : menuIcon}
+          alt="menu-button"
+          className={styles.menuBtn}
+          onClick={() => {
+            setMenuopen(!menuOpen);
+          }}
+        />
+        <ul
+          className={`${styles.menuitems} ${menuOpen && styles.menuOpen}`}
+          onClick={() => {
+            setMenuopen(false);
+          }}
+        >
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
         </ul>
       </div>
     </nav>
